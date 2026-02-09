e2633297b348022cc64992e658b3684d727f529a
iL0V3CH1L1 <painteddoorknob@gmail.com>
2026-02-09 10:42:19 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+const socket = new WebSocket('wss://mr-donut.onrender.com');
+
+socket.onopen = () => {
+  console.log('Connected to backend server!');
+  // You can send messages here, like requesting matchmaking:
+  socket.send(JSON.stringify({ type: 'findMatch' }));
+};
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  console.log('Message from backend:', data);
+  // Update your UI or game state based on backend messages
+};
+
+socket.onerror = (error) => {
+  console.error('WebSocket error:', error);
+};
+
+socket.onclose = () => {
+  console.log('Disconnected from backend');
+};
 
